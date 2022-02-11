4ddfe2126a7a8f4beacd8257df18ab5b22b21c65
francescarestante <restante.1745106@studenti.uniroma1.it>
2022-02-11 20:28:49 +0100

    modified user pages
    To fix elements in the page

--- my_flutter/lib/screens/profile/profile_page.dart
+++ my_flutter/lib/screens/profile/profile_page.dart
@@ -14,7 +14,223 @@ class ProfilePageState extends State<ProfilePage> {
         title: const Text("Profile"),
         automaticallyImplyLeading: false,
       ),
-      body: const Center(child: Text("My Profile Page")),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Image.asset("assets/images/parrot_cut.png",
+                      height: 110,
+                      fit: BoxFit
+                          .fitHeight), // overflowed - need to redemension the image!!
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 222,
+                    height: 220,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Nome",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        Text(
+                          "Professione",
+                          style: TextStyle(fontSize: 19, color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 26,
+              ),
+              Text(
+                "About",
+                style: TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Brief description or presentation of the worker",
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          // Image.asset(
+                          //     "assets/images/parrot_cut.png"), //field used for mapping, useful?
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Zona di lavoro",
+                                style: TextStyle(
+                                    color: Colors.black87.withOpacity(0.7),
+                                    fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Container(
+                                  width:
+                                      MediaQuery.of(context).size.width - 268,
+                                  child: Text(
+                                    "House # 2, Road # 5, Green Road Dhanmondi, Dhaka, Bangladesh",
+                                    style: TextStyle(color: Colors.grey),
+                                  ))
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          // Image.asset(
+                          //     "assets/images/parrot_cut.png"), //clock????
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Daily Practict",
+                                style: TextStyle(
+                                    color: Colors.black87.withOpacity(0.7),
+                                    fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Container(
+                                  width:
+                                      MediaQuery.of(context).size.width - 268,
+                                  child: Text(
+                                    '''Giorni di lavoro: ...''',
+                                    style: TextStyle(color: Colors.grey),
+                                  ))
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  //Image.asset(
+                  //  "assets/images/parrot_cut.png", //icon for the map here!!
+                  //  width: 180,
+                  // )
+                ],
+              ),
+              Text(
+                "Gallery or Price List(choose)",
+                style: TextStyle(
+                    color: Color(0xff242424),
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 22,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+                      decoration: BoxDecoration(
+                          color: Color(0xffFBB97C),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Color(0xffFCCA9B),
+                                borderRadius: BorderRadius.circular(16)),
+                            //child:
+                            //Image.asset("assets/images/parrot_cut.png")
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width / 2 - 130,
+                            child: Text(
+                              "List Of Schedule",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 17),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+                      decoration: BoxDecoration(
+                          color: Color(0xffA5A5A5),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Color(0xffBBBBBB),
+                                borderRadius: BorderRadius.circular(16)),
+                            //child:
+                            //Image.asset("assets/images/parrot_cut.png")
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width / 2 - 130,
+                            child: Text(
+                              "???",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 17),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
