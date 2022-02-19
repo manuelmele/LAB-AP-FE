038dc80cd72de10acba6ca6049b892c1967d3d81
@emanuele.mele <emanuele.mele@keypartner.com>
2022-02-19 14:32:04 +0100

    homepage filters added image

--- my_flutter/lib/services/filters_service.dart
+++ my_flutter/lib/services/filters_service.dart
@@ -7,8 +7,8 @@ import 'package:image_picker/image_picker.dart';
 import 'package:wefix/models/user_model.dart';
 
 //[BE] server.port must be localhost:8000
-//String baseUrl = '10.0.2.2:8000'; //indirizzo per emulatore
-String baseUrl = '192.168.1.9:8000'; //indirizzo IP laura
+String baseUrl = '10.0.2.2:8000'; //indirizzo per emulatore
+//String baseUrl = '192.168.1.9:8000'; //indirizzo IP laura
 
 Future<List<UserModel>> filterByCategory(String _jwt, String _category) async {
   final queryParameters = {
