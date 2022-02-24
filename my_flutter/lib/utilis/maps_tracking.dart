ed9c836b053791ead498e793fe697904d7ef2ed6
@emanuele.mele <emanuele.mele@keypartner.com>
2022-02-24 20:33:30 +0100

    Showing position map

--- my_flutter/lib/utilis/maps_tracking.dart
+++ my_flutter/lib/utilis/maps_tracking.dart
@@ -5,23 +5,59 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:wefix/services/meetings_service.dart';
+import 'package:wefix/utilis/allert_dialogs.dart';
 
 class TrackingWidget extends StatefulWidget {
+  final String userJWT;
+  final int meetingId;
+
+  const TrackingWidget(
+      {Key? key, required this.userJWT, required this.meetingId})
+      : super(key: key);
   @override
   TrackingWidgetState createState() => TrackingWidgetState();
 }
 
 class TrackingWidgetState extends State<TrackingWidget> {
   LatLng? userPosition = LatLng(41.9028, 12.4964);
+  Marker? marker;
+
+  Timer? timer;
 
   GoogleMapController? _googleMapController;
 
   @override
   void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
     if (_googleMapController != null) {
       _googleMapController!.dispose();
     }
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(
+        Duration(seconds: 2),
+        (Timer t) => getMeetingPosition(widget.userJWT, widget.meetingId)
+                .then((position) {
+              Marker newMarker = Marker(
+                markerId: MarkerId(widget.meetingId.toString()),
+                icon: BitmapDescriptor.defaultMarkerWithHue(
+                    BitmapDescriptor.hueAzure),
+                position: LatLng(position.lat!, position.lng!),
+              );
+
+              if (timer != null && timer!.isActive) {
+                setState(() {
+                  marker = newMarker;
+                });
+              }
+            }));
   }
 
   void requestLocationPermission() async {
@@ -36,17 +72,21 @@ class TrackingWidgetState extends State<TrackingWidget> {
   @override
   Widget build(BuildContext context) {
     requestLocationPermission();
-
-    return GoogleMap(
-      initialCameraPosition: CameraPosition(
-        target: userPosition!,
-        zoom: 12,
-        tilt: 50.0,
-      ),
-      myLocationButtonEnabled: true,
-      myLocationEnabled: true,
-      compassEnabled: true,
-      onMapCreated: (controller) => _googleMapController = controller,
-    );
+    if (marker != null) {
+      return GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: userPosition!,
+          zoom: 12,
+          tilt: 50.0,
+        ),
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        compassEnabled: true,
+        markers: {marker!},
+        onMapCreated: (controller) => _googleMapController = controller,
+      );
+    } else {
+      return Center(child: CircularProgressIndicator());
+    }
   }
 }
